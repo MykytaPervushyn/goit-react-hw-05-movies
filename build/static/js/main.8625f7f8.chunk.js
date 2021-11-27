86e62d184f9d0d44781fa27979ec36f87b60eacf
MykytaPervushyn <80350205+MykytaPervushyn@users.noreply.github.com>
2021-11-27 14:49:56 +0200

    Створює базову структуру

---
+++ build/static/js/main.8625f7f8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var c=n(1),i=n.n(c),o=n(3),r=n.n(o),s=(n(8),n(9),n(0));function h(){return Object(s.jsx)("div",{children:Object(s.jsx)("h1",{children:"TEST"})})}r.a.render(Object(s.jsx)(i.a.StrictMode,{children:Object(s.jsx)(h,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.8625f7f8.chunk.js.map
--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import { useState } from 'react';
 import './App.css';
-import Searchbar from './components/Searchbar/Searchbar';
-import ImageGallery from './components/ImageGallery/ImageGallery';
-import Modal from './components/Modal/Modal';
 
 export default function App() {
-  const [showModal, setShowModal] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [largeImageURL, setLargeImageURL] = useState('');
-
-  const toggleModal = (largeImageURL) => {
-    setShowModal(!showModal);
-    setLargeImageURL(largeImageURL);
-  };
 
   return (
     <div>
-        <Searchbar onSubmit={setSearchQuery}/>
-      <ImageGallery
-          toggleModal={toggleModal}
-          searchQuery={searchQuery}
-
-        />
-        {showModal && <Modal
-          toggleModal={toggleModal}
-          largeImageURL={largeImageURL} />}
+        <h1>TEST</h1>
     </div>
   )
 }
